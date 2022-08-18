b0ba93b6d77a5c0b0cc61f419a0bd7425281cb31
jayvinb <jayvin@shoptype.com>
2022-08-18 12:25:49 +0530

    update form design

--- create-vandor-form.php
+++ create-vandor-form.php
@@ -1,5 +1,4 @@
 <?php
-
 //short code [create-storemain]
 function create_storemain() {
 $message='';
@@ -30,7 +29,7 @@ try {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $result = curl_exec($ch);
     $st_user = json_decode($result);
-
+    curl_close($ch);
 // Close cURL session handle
   }
   catch(Exception $e) {
@@ -44,23 +43,37 @@ try {
 if( empty( $result ) ) {$message='invalid token'; echo '<div class"statusMsg"><p>'.$message.'</p></div>'; return false;}
  
  ?>
+<div class="store-header">
+<div class="store-header-banner"></div>
+<div class="user-info">
+<div class="user-image">
+<img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png">
+	</div>
+<div class="user-des">
+
+<div class="user-name">
+@ <?php echo $st_user->name; ?>
+	</div>
+	</div>
+
+	</div>
+</div>
 <!-- Status message -->
 <div class="statusMsg"></div>
 
 <!-- File upload form -->
 <div class="col-lg-12">
     <form id="storeForm" enctype="multipart/form-data" method='post' action=''>
-        <div class="form-group">
-            <label for="name">Email</label>
-          <input type="text" class="form-control" id="store-email" name="store-email" value="<?php echo $st_user->email ?>" readonly />
-        </div>
         
         <div class="form-group">
+            
             <label for="name">Store Name</label>
           <input type="text" class="form-control" id="store-name" name="store-name" value="<?php echo $st_user->vendors[0]->name; ?>" readonly />
         </div>
+        <div class="form-flex">
+        
         <div class="form-group">
-              <label for="currency">Choose a currency:</label>
+              <label for="currency">Currency</label>
   <select id="store-currency" name="store-currency" class="form-select">
     <option value="CAD">CAD</option>
     <option value="USD">USD</option>
@@ -69,17 +82,168 @@ if( empty( $result ) ) {$message='invalid token'; echo '<div class"statusMsg"><p
   </select>
         </div>
         <div class="form-group">
+         
+              <label for="Address">Address</label>
+  <input type="text" name="store-Address" class="form-address">
+        </div>
+        
+      </div>
+      <div class="form-group">
+         
+  <input type="text" name="store-Address1" class="form-address1">
+        </div>
+
+        <div class="form-flex">
+        
+        <div class="form-group">
+         
+              <label for="city">City</label>
+  <input type="text" name="store-city" class="form-city">
+        </div>
+        <div class="form-group">
+         
+              <label for="zip">Zip</label>
+  <input type="text" name="store-zip" class="form-zip">
+        </div>
+        
+      </div>
+
+      <div class="form-flex">
+        
+        <div class="form-group">
+         
+              <label for="Country">Country</label>
+  <select id="st-country" name="country" class="form-control">
+<option value="">Select country</option>
+</select>
+        </div>
+        <div class="form-group">
+         
+              <label for="State">State</label>
+<select id="st-state" name="State" class="form-control"></select>
+    
+      </div>
+        
+      </div>
+<div class="file-upload">
+  <label for="store-image">Store image</label>
+
+  <div class="image-upload-wrap">
+    <input class="file-upload-input" type='file' name='fileToUpload' id='fileToUpload' onchange="readURL(this);" accept="image/*" />
+    <div class="drag-text">
+      JPG. PNG
+<br>
+You can also upload files
+<br>
+		<span style="text-decoration:underline;color:blue;padding-top:15px;">Click here</span>
+<br>
+    </div>
+  </div>
+  <div class="file-upload-content">
+    <img class="file-upload-image" src="#" alt="your image" />
+    <div class="image-title-wrap">
+      <button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
+    </div>
+  </div>
+</div>
+        <div class="form-group">
           
             <input type="hidden" class="form-control" id="store-token" name="store-token" value="<?php echo $_GET['token']?>" />
            
         </div>
-        <input type="submit" name="createstore" class="btn btn-success submitBtn" value="Crete Store"/>
+        <input type="submit" name="createstore" class="btn btn-success submitBtn" value="Save"/>
     </form>
 </div>
+
 <style>
+.store-header
+	{
+		}
+.store-header-banner
+	{
+margin-left:-30%;
+
+margin-right:-16.5%;
+width:100v;
+height: 250px;
+
+background: #D9D9D9;
+	}
+
+.user-info{
+position: relative;
+width: 100%;
+height: 150px;
+top: -75px;
+margin:auto;
+gap:30px;
+background: #FFFFFF;
+box-shadow: 0px 5px 23px rgba(0, 0, 0, 0.08);
+border-radius: 10px;
+display:flex;
+padding:20px;
+}
+.user-image{
+width: 110px;
+height: 110px;
+
+background: #FFFFFF;
+border-radius: 5px;
+	}
+.user-des{
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+ margin-top: 10px;
+font-size: 20px;
+line-height: 30px;
+	}
+/* identical to box height */
+
+.submitBtn
+	{
+margin:auto !important;
+	width: 100px;
+    height: 46px;
+    left: 485px;
+    top: 983px;
+    border: 1px solid #959090;
+    border-radius: 10px;
+    color: #686868;
+    text-align: center;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+	}
+color: #808080;
+}
+.user-name{
+font-family: 'Poppins';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 30px;
+/* identical to box height */
+
+
+color: #808080;
+}
+  .form-flex
+  {
+    display: flex;
+  }
 .form-group {
-    margin: 25px;
-    width: 50%;
+    margin: 10px;
+   
+}
+.col-lg-12
+	{
+max-width: 600px;
+ margin: auto;
+	}
+.form-flex .form-group {
+width:100%;	
 }
 input.btn.btn-success.submitBtn {
     margin-left: 2%;
@@ -87,14 +251,113 @@ input.btn.btn-success.submitBtn {
 input#store-image {
     margin-top: 10px;
 }
+input[type=date], input[type=email], input[type=number], input[type=password], input[type=search], input[type=tel], input[type=text], input[type=url], select, textarea{
+border-radius:5px;
+border-color:#959090;
+border-width:2px;
+padding-left:10px;
+padding-right:10px;
+
+		
+	}
+.file-upload {
+  margin: 0 auto;
+  padding: 20px;
+}
+
+label {
+    padding-bottom: 10px;
+	}
+
+.file-upload-content {
+  display: none;
+  text-align: center;
+}
+
+.file-upload-input {
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  outline: none;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.image-upload-wrap {
+  margin-top: 20px;
+  border: 4px dashed #B8B7B7;
+  position: relative;
+}
+
+.image-dropping,
+.image-upload-wrap:hover {
+  background-color: #1FB264;
+  border: 4px dashed #B8B7B7;
+}
+
+.image-title-wrap {
+  padding: 0 15px 15px 15px;
+  color: #222;
+}
+
+.drag-text {
+padding-top:35px;
+  text-align: center;
+padding-bottom:35px;
+}
+
+.drag-text h3 {
+  font-weight: 100;
+  text-transform: uppercase;
+  color: #15824B;
+  padding: 60px 0;
+}
+
+.file-upload-image {
+  max-height: 200px;
+  max-width: 200px;
+  margin: auto;
+  padding: 20px;
+}
+
+.remove-image {
+  width: 200px;
+  margin: 0;
+  color: #fff;
+  background: #cd4535;
+  border: none;
+  padding: 10px;
+  border-radius: 4px;
+  border-bottom: 4px solid #b02818;
+  transition: all .2s ease;
+  outline: none;
+  text-transform: uppercase;
+  font-weight: 700;
+}
+
+.remove-image:hover {
+  background: #c13b2a;
+  color: #ffffff;
+  transition: all .2s ease;
+  cursor: pointer;
+}
+
+.remove-image:active {
+  border: 0;
+  transition: all .2s ease;
+}
 </style>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/shoptype/Shoptype-JS@2.7.11/shoptype.js"></script>
 <script>
+
   function setStoreUrl()
   {
 var storeCurrency= jQuery('#store-currency').val();
 if(storeCurrency === 'CAD')
 {
- jQuery('#storeForm').attr('action', "/test1")
+ jQuery('#storeForm').attr('action', "http://localhost/coseller/add-store/?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRob3JpemVkIjp0cnVlLCJpZCI6ImU3YzRkNWYzLWVjMTEtZjhhNy00ZmFhLWUwNDc3NGRhYmFhYiIsInVzZXJJZCI6ImJmMmU5NDBlLTQ4MTgtNDBhMy1iNjE3LTBkZTRiYzY4ZjA4YiIsInVzZXJUeXBlIjoidmVuZG9yIiwiaXNzIjoic2hvcC10eXBlIn0.BbfhRpzmspL7d6U9XR4SdfV33CkGLv-cnRKZgUeUGtE")
   }
  if(storeCurrency === 'USD')
 {
@@ -108,13 +371,48 @@ if(storeCurrency === 'CAD')
   }   
 }
 jQuery(document).ready(function(){
+	
    setStoreUrl();
     jQuery('#store-currency').change(setStoreUrl);
+	setCountry();
 
     });
   
-    
+ function readURL(input) {
+  if (input.files && input.files[0]) {
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      jQuery('.image-upload-wrap').hide();
+
+      jQuery('.file-upload-image').attr('src', e.target.result);
+      jQuery('.file-upload-content').show();
+
+      jQuery('.image-title').html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+
+  } else {
+    removeUpload();
+  }
+}
+
+function removeUpload() {
+  jQuery('.file-upload-input').replaceWith(jQuery('.file-upload-input').clone());
+  jQuery('.file-upload-content').hide();
+  jQuery('.image-upload-wrap').show();
+}
+jQuery('.image-upload-wrap').bind('dragover', function () {
+		jQuery('.image-upload-wrap').addClass('image-dropping');
+	});
+	jQuery('.image-upload-wrap').bind('dragleave', function () {
+		jQuery('.image-upload-wrap').removeClass('image-dropping');
+});
+   
 </script>
+
   <?php
 //<div class="statusMsg"><p>Faild to crete store</p></div>
 //<?php 
--- create-vandor.php
+++ create-vandor.php
@@ -3,12 +3,18 @@
 //short code [create-store]
 function create_store() {
 
-    //Check if user come from form
+	//Check if user come from form
     if (isset($_POST['createstore'])) {
         $message = '';
-        $backendUrl = 'https://dev-backend.shoptype.com';
+        $backendUrl = 'https://beta-backend.shoptype.com';
         if (isset($_POST['store-token'])) {
             $token = $_POST['store-token'];
+			$street_1= isset($_POST['store-Address']) ? $_POST['store-Address'] :'';
+			$street_2= isset($_POST['store-Address1']) ? $_POST['store-Address1'] :'';
+			$city= isset($_POST['store-city']) ? $_POST['store-city'] :'';
+			$zip= isset($_POST['store-zip']) ?$_POST['store-zip'] :'';
+			$state= isset($_POST['State']) ?$_POST['State'] :'';
+			$country= isset($_POST['country']) ?$_POST['country'] :'';
         } else {
             $message = 'invalid token';
             echo '<div class"statusMsg"><p>' . $message . '</p></div>';
@@ -17,7 +23,7 @@ function create_store() {
 
       //Authorised tokon for creating dokan store
         try {
-            $headers = array("authorization: " . $token, "origin:https://dev.shoptype.com", "referer: https://dev.shoptype.com");
+            $headers = array("authorization: " . $token, "origin:https://beta.shoptype.com", "referer: https://beta.shoptype.com");
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, "{$backendUrl}/me");
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -43,54 +49,116 @@ function create_store() {
         if (empty($user)) {
             $user_id = wp_insert_user(array('user_login' => $st_user->email, 'user_pass' => $token, 'user_email' => $st_user->email, 'first_name' => $st_user->name, 'display_name' => $st_user->name, 'role' => 'seller'));
             if (!empty($user_id)) {
-                $dokan_settings = array('store_name' => $st_user->vendors[0]->name);
+				$dokan_settings = array(
+				'store_name' => $st_user->vendors[0]->name,
+				'address'=>array
+                (
+                    'street_1' => $street_1,
+                    'street_2' =>$street_2, 
+                    'city' => $city,
+                    'zip' => $zip,
+                    'state' =>$state, 
+                    'country' => $country
+                ),);
                 update_user_meta($user_id, 'dokan_enable_selling', 'yes');
                 update_user_meta($user_id, 'dokan_profile_settings', $dokan_settings);
                 update_user_meta($user_id, 'dokan_store_name', $st_user->vendors[0]->name);
             }
+  //upload store image
+            
+
+
         } else {
             $user->add_role('seller');
-            $dokan_settings = array('store_name' => $st_user->vendors[0]->name);
+            $dokan_settings = array(
+				'store_name' => $st_user->vendors[0]->name,
+				'address'=>array
+                (
+                    'street_1' => $street_1,
+                    'street_2' =>$street_2, 
+                    'city' => $city,
+                    'zip' => $zip,
+                    'state' =>$state, 
+                    'country' => $country
+                ),);
             update_user_meta($user->ID, 'dokan_enable_selling', 'yes');
             update_user_meta($user->ID, 'dokan_profile_settings', $dokan_settings);
             update_user_meta($user->ID, 'dokan_store_name', $st_user->vendors[0]->name);
         }
         $user = get_user_by('email', "{$st_user->email}");
-        //print_r($user);
-            $site_url = site_url('', 'https');
+        if(@$_POST['createstore'])
+ {
+ $file_name = $_FILES['fileToUpload']['name'];
+                $file_temp = $_FILES['fileToUpload']['tmp_name'];
+
+                $upload_dir = wp_upload_dir();
+                $image_data = file_get_contents( $file_temp );
+                $filename = basename( $file_name );
+                $filetype = wp_check_filetype($file_name);
+                $filename = time().'.'.$filetype['ext'];
+
+                if ( wp_mkdir_p( $upload_dir['path'] ) ) {
+                  $file = $upload_dir['path'] . '/' . $filename;
+                }
+                else {
+                  $file = $upload_dir['basedir'] . '/' . $filename;
+                }
+
+                file_put_contents( $file, $image_data );
+                $wp_filetype = wp_check_filetype( $filename, null );
+                $attachment = array(
+                  'post_mime_type' => $wp_filetype['type'],
+                  'post_title' => sanitize_file_name( $filename ),
+                  'post_content' => '',
+                  'post_status' => 'inherit'
+                );
+
+                $attach_id = wp_insert_attachment( $attachment, $file );
+                require_once( ABSPATH . 'wp-admin/includes/image.php' );
+                $attach_data = wp_generate_attachment_metadata( $attach_id, $file );
+                wp_update_attachment_metadata( $attach_id, $attach_data );
+
+                echo $attach_id;
+$atturl=wp_get_attachment_url( $attach_id);
+
+ }
         
-        if (isset($user->ID)) {
+        //print_r($user);
+			$site_url = site_url('', 'https');
+        
+		if (isset($user->ID)) {
             wp_clear_auth_cookie();
             wp_set_current_user($user->ID);
             wp_set_auth_cookie($user->ID);
             $redirect_to = "{$site_url}/dashboard";
             wp_safe_redirect($redirect_to);
                     }
+		$userlogin = wp_signon( array( 'user_login' => $user->user_login, 'user_password' => $token), false );
         //Send vandor data to the shoptype
         try {
             $woocomercedata = array(
-                'storeName' => $site_url, 
-                'storeHostUrl' => $site_url, 
-                'consumerKey' => "ck_86a34cccae27a5babe1b3e07240182efe7fc5625", 
-                'consumerSecret' => "cs_db669378a1bf27ec197b1127ddd36b6f19159590", 
-                'dokan_vendor_id' => strval($user->ID), 
-                'dokan_vendor_name' => $st_user->vendors[0]->name, 
-                'enableCheckoutShoptype' => true, 
-                'shippingServiceId' => "001",
-                'restrictions'=>array('isAdult'=> "false", 'isAgeRestricted'=>"false")
+            	'storeName' => $site_url, 
+            	'storeHostUrl' => $site_url, 
+            	'consumerKey' => "ck_86a34cccae27a5babe1b3e07240182efe7fc5625", 
+            	'consumerSecret' => "cs_db669378a1bf27ec197b1127ddd36b6f19159590", 
+            	'dokan_vendor_id' => strval($user->ID), 
+            	'dokan_vendor_name' => $st_user->vendors[0]->name, 
+            	'enableCheckoutShoptype' => true, 
+            	'shippingServiceId' => "001",
+            	'restrictions'=>array('isAdult'=> "false", 'isAgeRestricted'=>"false")
 
         );
                         $payload = json_encode($woocomercedata);
-            print_r($payload);
+            //print_r($payload);
             
             // Prepare new cURL resource
-            $ch = curl_init('https://dev-backend.shoptype.com/store/woo-commerce');
+            $ch = curl_init('https://beta-backend.shoptype.com/store/woo-commerce');
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLINFO_HEADER_OUT, true);
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
             // Set HTTP Header for POST request
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "authorization:".$token,"origin:https://dev.shoptype.com", "referer: https://dev.shoptype.com"));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json", "authorization:".$token,"origin:https://beta.shoptype.com", "referer: https://beta.shoptype.com"));
             // Submit the POST request
             $result = curl_exec($ch);
             $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
